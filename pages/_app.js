455ead7bb72be6584005ff34cc2c1570e81fa5ce
alejandro <diazbrandonalejandro@gmail.com>
2023-03-29 13:10:48 +0200

    First draft

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+        <Head>
+        <title>News Appi</title>
+        <meta name="description" content="News App from Api" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header> News App</header>
+
+  <Component {...pageProps} />
+
+
+  </>
 }
